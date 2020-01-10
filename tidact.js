03fda80a97788a25027043edd7dd01f4773c2630
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2020-01-10 12:57:33 +0100

    solve bug from bad use of map

--- tidact.js
+++ tidact.js
@@ -1,23 +1,73 @@
 function createElement(type, props, ...children) {
+    function createTextElement(text) {
+        return {
+            type: "TEXT_ELEMENT",
+            props: {
+                nodeValue: text,
+                children: []
+            }
+        };
+    }
+
     return {
         type,
         props: {
             ...props,
             children: children.map(c =>
-                typeof c === "object" ? c : createTextNode(c)
+                typeof c === "object" ? c : createTextElement(c)
             )
         }
     };
 }
 
-function createTextNode(text) {
-    return {
-        type: "TEXT_ELEMENT",
-        props: {
-            nodeValue: text,
-            children: []
+function performUnitOfWork(fiber) {
+    function createDom(fiber) {
+        const dom =
+            fiber.type === "TEXT_ELEMENT" ?
+            document.createTextNode("") :
+            document.createElement(fiber.type);
+
+        const isProp = k => k !== "children";
+        Object.keys(fiber.props)
+            .filter(isProp)
+            .forEach(name => {
+                dom[name] = fiber.props[name];
+            });
+
+        return dom;
+    }
+
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber);
+    }
+    // create new fibers
+    const elements = fiber.props.children;
+    const fibers = elements
+        .map(el => ({
+            type: el.type,
+            props: el.props,
+            parent: fiber,
+            dom: null // We need to finish this before adding sibling
+        }))
+        .map((fiber, index, arr) => ({
+            ...fiber,
+            sibling: arr[index + 1] || null /* last one is index out bounds but it's fine*/
+        }));
+
+    fiber.child = fibers[0];
+
+    // return next unit of work
+    if (fiber.child) {
+        return fiber.child;
+    }
+
+    let nextFiber = fiber;
+    while (nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling;
         }
-    };
+        nextFiber = nextFiber.parent;
+    }
 }
 
 let nextUnitOfWork = null;
@@ -38,69 +88,22 @@ function workLoop(deadline) {
 }
 
 function commitRoot() {
+    function commitWork(fiber) {
+        if (!fiber) {
+            return;
+        }
+
+        const domParent = fiber.parent.dom;
+        domParent.appendChild(fiber.dom);
+        commitWork(fiber.child);
+        commitWork(fiber.sibling);
+    }
+
     commitWork(wipRoot.child);
     wipRoot = null;
 }
 
-function commitWork(fiber) {
-    if (!fiber) {
-        return;
-    }
-
-    console.dir(fiber);
-    // TODO something is funky here, debug
-    const domParent = fiber.parent.dom;
-    domParent.appendChild(fiber.dom);
-    commitWork(fiber.child);
-    commitWork(fiber.sibling);
-}
-
-function performUnitOfWork(fiber) {
-    if (!fiber.dom) {
-        fiber.dom = createDom(nextUnitOfWork);
-    }
-    // create new fibers
-    const elements = fiber.props.children;
-    const fibers = elements.map((el, index, arr) => ({
-        type: el.type,
-        props: el.props,
-        parent: fiber,
-        dom: null,
-        sibling: arr[index + 1] || null /* last one is index out bounds but it's fine*/
-    }));
-    fiber.child = fibers[0];
-
-    // return next unit of work
-    if (fiber.child) {
-        return fiber.child;
-    }
-
-    let nextFiber = fiber;
-    while (nextFiber) {
-        if (nextFiber.sibling) {
-            return nextFiber.sibling;
-        }
-        nextFiber = nextFiber.parent;
-    }
-}
-
 requestIdleCallback(workLoop);
-
-function createDom(fiber) {
-    const dom =
-        fiber.type === "TEXT_ELEMENT" ?
-        document.createTextNode("") :
-        document.createElement(fiber.type);
-
-    const isProp = k => k !== "children";
-    Object.keys(fiber.props)
-        .filter(isProp)
-        .forEach(name => {
-            dom[name] = fiber.props[name];
-        });
-
-    return dom;
-}
 
 function render(element, container) {
     wipRoot = { dom: container, props: { children: [element] } };
